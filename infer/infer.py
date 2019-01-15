1eb54f29baa26a42ac00b561dc282c2dc15d49d9
sxchen <sxchen@163.com>
2019-01-15 20:46:36 +0800

    add an inference script in ./infer

---
+++ infer/infer.py
@@ -0,0 +1,89 @@
+
+# coding: utf-8
+
+import os
+import sys
+import math
+import utils
+import random
+import numpy as np
+from tqdm import tqdm
+import tensorflow as tf
+from datetime import datetime
+
+from preprocessing import preprocessing_factory
+slim = tf.contrib.slim
+import model
+from PIL import Image
+import cPickle as pkl
+
+class InferUtil(object):
+    def __init__(self, model_ckpt):
+
+
+        utils.set_gpu(1)
+
+        num_classes = 340
+        image_size = 128
+        self._is = image_size
+
+        self.images = tf.placeholder(shape=[None, image_size, image_size, 1], dtype=tf.uint8)
+
+        image_prep_fn = preprocessing_factory.get_preprocessing('inception_v1', is_training=False)
+        images_preped = image_prep_fn(self.images, None, None)
+
+        class_logits = model.build_net(images_preped, num_classes, False)
+
+        self.class_probs = tf.nn.softmax(class_logits)
+        #preds = tf.argmax(class_probs, axis=-1)
+
+        saver = tf.train.Saver(tf.global_variables())
+        self.sess = tf.Session()
+        saver.restore(self.sess, model_ckpt)
+
+        cls_id_map = pkl.load(file('class_id_map.pkl', 'rb'))
+        self.id_cls_map = {}
+        for k in cls_id_map:
+            self.id_cls_map[cls_id_map[k]] = k
+
+
+    def infer(self, image, k):
+        """
+        Parameters
+        ----------
+        image : ndarray of uint8
+            image can be 2D, 3D or 4D tensor.
+            2D: [h, w]
+            3D: [h, w, 1]
+            4D: [b, h, w, 1]
+
+        k : int
+            top-k predictions to return
+
+        Returns
+        -------
+        nested list of length 1 or b:
+            returns a list for each image in the batch,
+            in which items are (prob, class_name) tuples, sorted by prob in descending order.
+        """
+
+        if image.ndim in (2,3):
+            image = np.reshape(image, [1, self._is, self._is, 1])
+
+        probs = self.sess.run(self.class_probs, {self.images: image})
+
+        ret = []
+        for prob in probs:
+            idxs = np.argsort(prob)[::-1][:k]
+            ret.append([(prob[i], self.id_cls_map[i]) for i in idxs])
+        return ret
+
+if __name__ == "__main__":
+    iu = InferUtil('./ckpt/model-99001')
+
+    img = np.array(Image.open('bee.png'))
+
+    print img.shape
+
+    print iu.infer(img, 5)
+
---
+++ infer/utils.py
@@ -0,0 +1,43 @@
+import os
+import sys
+import GPUtil
+import operator
+import tensorflow as tf
+
+def print_file(f):
+    for l in open(f, 'r').readlines():
+        sys.stdout.write(l)
+
+def write_file(s,n):
+    with open(n, 'w') as f:
+        f.write(s)
+
+def cfg_dict2tuple(d):
+    cfg = namedtuple('Config', d.keys())
+    for k in d.keys():
+        exec('cfg.%s = d[k]' % k)
+    return cfg
+
+def set_gpu(num_gpu):
+    deviceIDs = GPUtil.getAvailable(order = 'first', limit = num_gpu, maxMemory = 0.2)
+    if len(deviceIDs) < num_gpu:
+        print 'No enough GPU.'
+        sys.exit(0)
+    else:
+        gpu_id = ','.join([str(d) for d in deviceIDs])
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = gpu_id
+
+
+def combine_gradients(tower_grads):
+    filtered_grads = [[x for x in grad_list if x[0] is not None] for grad_list in tower_grads]
+    final_grads = []
+    for i in xrange(len(filtered_grads[0])):
+        grads = [filtered_grads[t][i] for t in xrange(len(filtered_grads))]
+        grad = tf.stack([x[0] for x in grads], 0)
+        grad = tf.reduce_sum(grad, 0)
+        final_grads.append((grad, filtered_grads[0][i][1],))
+
+    return final_grads
+
+
